5a6ecca65394b89a7723bfda1518b440e6a5884b
jeffwcx <jeffwcx@icloud.com>
2024-12-04 15:43:08 +0800

    chore: update author

--- website/astro.config.js
+++ website/astro.config.js
@@ -5,7 +5,7 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-i18next.yassinedoghri.com/",
+  site: "https://jeffwcx.github.io/astro-i18next/",
   integrations: [
     sitemap({
       i18n: {
